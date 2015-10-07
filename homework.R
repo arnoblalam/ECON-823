82ba220e6d36c467ace9efdfb163c2ae6b19df94
Arnob Alam <arnoblalam@gmail.com>
2015-10-07 04:41:03 -0400

    Finish HW

--- homework.R
+++ homework.R
@@ -1,16 +1,33 @@
-# library(lambda.r)
-# pDF(x, y) %when% {
-#   0 < x
-#   0 < y
-#   x + y < 1
-# } %as% 24*x*y
-# pDF(x, y) %as% 0
-# 
-# integrate(function(y) {
-#   sapply(y, function(y) {
-#     integrate(Vectorize(function(x) Vectorize(pDF(x, y))), lower=0, upper = 1)$value
-#   })
-# }, lower=-0, upper=1)
+# Problem 7 #
+library(lambda.r)
+
+# Verifying Problem 2 #
+pDF(x, y) %when% {
+  0 < x
+  0 < y
+  x + y < 1
+} %as% {24*x*y}
+pDF(x, y) %as% 0
+
+integrate(function(y) {
+  sapply(y, function(y) {
+    integrate(Vectorize(function(x) Vectorize(pDF(x, y))), lower=0, upper = 0.5-y)$value
+  })
+}, lower=-0, upper=Inf)
+
+# Verifying Problem 4 #
+
+pDF(x, y) %when% {
+  x >= 0
+  y >= 0
+} %as% {1 - exp(-x) - exp(-y) + exp(-x-y)}
+pDF(x, y) %as% 0
+
+integrate(function(y) {
+  sapply(y, function(y) {
+    integrate(Vectorize(function(x) Vectorize(pDF(x, y))), lower=0, upper = 3-y)$value
+  })
+}, lower=-3, upper=Inf)
 
 
 # Problem 8 #
@@ -21,10 +38,13 @@ randomDraws <- runif(1E3, min = 0, max = 1)
 
 results.simulated <- sapply(randomDraws, simulated.pDF)
 
-results.simulated.sq = results.simulated^2
+results.simulated.sq = results.simulated
 
 results.direct <- rexp(1E3)
 
+hist(results.direct, freq=FALSE, col="red", main="Actual results")
 
-hist(results.simulated.sq, freq = FALSE, col="green")
-hist(results.direct, freq=FALSE, col="red", add=TRUE)
+hist(results.simulated.sq, freq = FALSE, col="green", main="Simulated Results")
+
+mean(results.simulated.sq)
+mean(results.direct)
